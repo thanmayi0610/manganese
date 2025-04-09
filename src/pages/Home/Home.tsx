e7304ba1388c2f792281d836bf51f0fb3eb3a549
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-09 16:40:09 +0530

    Add BearCounter component to Home page for bear tracking functionality

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,11 +14,14 @@
 import React from "react";
 import "./Home.css";
 import NavigationBar from "../../components/NavigationBar";
+import BearCounter from "./components/bears/bears";
+import Tigers from "./components/tigers/tiger";
 
 const Home = (): React.ReactNode => {
   return (
     <div className="home-root">
       <NavigationBar />
+      <BearCounter />
     </div>
   );
 };
