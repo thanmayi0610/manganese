4cffb4e761e64bb8edfc291a51f6fc91a62604fc
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-09 16:38:52 +0530

    Refactor NavigationBar component to include animal data and improve structure

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -14,33 +14,59 @@
 import { NavLink, useLocation } from "react-router";
 import "./NavigationBar.css";
 
+import { useAnimalsStore } from "../hooks/animal-store/animals-store";
+
 const NavigationBar = (): React.ReactNode => {
   const location = useLocation();
   const currentRoute = location.pathname;
+  const { animals } = useAnimalsStore();
 
   return (
-    <ul className="navigation-bar-links">
-      <li>
-        <NavLink to="/product" className={currentRoute == "/product" ? "navigation-link-selected" : ""}>
-          Product
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/developers" className={currentRoute == "/developers" ? "navigation-link-selected" : ""}>
-          Developers
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/enterprise" className={currentRoute == "/enterprise" ? "navigation-link-selected" : ""}>
-          Enterprise
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/pricing" className={currentRoute == "/pricing" ? "navigation-link-selected" : ""}>
-          Pricing
-        </NavLink>
-      </li>
-    </ul>
+    <div className="navigation-bar-root">
+      <ul className="navigation-bar-links">
+        <li>Bears:{animals}</li>
+        <li>
+          <NavLink
+            to="/product"
+            className={
+              currentRoute == "/product" ? "navigation-link-selected" : ""
+            }
+          >
+            Product
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/developers"
+            className={
+              currentRoute == "/developers" ? "navigation-link-selected" : ""
+            }
+          >
+            Developers
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/enterprise"
+            className={
+              currentRoute == "/enterprise" ? "navigation-link-selected" : ""
+            }
+          >
+            Enterprise
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/pricing"
+            className={
+              currentRoute == "/pricing" ? "navigation-link-selected" : ""
+            }
+          >
+            Pricing
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 };
 
